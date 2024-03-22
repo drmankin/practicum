2ab424fa7f8434809c9236fb08fdcd8593cf45c2
drmankin <j.mankin@sussex.ac.uk>
2024-03-22 10:34:17 +0000

    Update Qualtrics workshop, add workbook and datasets

--- scripts/on_render/gen_data_wkbks.R
+++ scripts/on_render/gen_data_wkbks.R
@@ -12,22 +12,28 @@ data_info <- tibble::tribble(
   "anx_data.csv", "Terry, Lea, & Field (in prep)", "https://osf.io/8jk5v/", "Dataset shared for teaching purposes. Note that demographics in this dataset are SIMULATED FOR TEACHING PURPOSES and are NOT real data.",
   "anx_scores_data.csv", "Terry, Lea, & Field (in prep)", "https://osf.io/8jk5v/", "Dataset shared for teaching purposes, with mean subscale scores instead of individual items. Note that demographics in this dataset are SIMULATED FOR TEACHING PURPOSES and are NOT real data.",
   "bp_data.csv", "Simon & Hurst (2021)", "https://sussex.figshare.com/articles/dataset/Dataset_for_paper_Body_Positivity_but_not_for_everyone/9885644", "Dataset publicly available. Note that this subset of the public data has had ID variables and missing values randomly introduced for teaching purposes.",
-  "mil_data.sav", "Eldarwish et al, (in prep)", "https://profiles.sussex.ac.uk/p323096-vlad-costin", "Dataset expected to be publicly available in the future. Note that this dataset is entirely simulated, based on existing real data."
+  "mil_data.sav", "Eldarwish et al., (in prep)", "https://profiles.sussex.ac.uk/p323096-vlad-costin", "Dataset expected to be publicly available in the future. Note that this dataset is entirely simulated, based on existing real data.",
+  "mil_data_wkshp.sav", "Eldarwish et al., (in prep)", "https://profiles.sussex.ac.uk/p323096-vlad-costin", "Version of mil_data.sav with additional simulated variables for Qualtrics workshop use, contributed by Dr Dan Evans"
 ) |> 
   dplyr::mutate(
     cite_link = paste0("<a href='", cite, "'>", source, "</a>")
   ) |> 
-  dplyr::select(data_file, cite_link, notes)
-
-data_tab <- tibble::tibble(
-  data_file = list.files(data_path, pattern = "data\\..*?$"),
-  link = paste0("<a href='https://raw.githubusercontent.com/drmankin/practicum/master/data/", 
-                data_file, "'>Download ", data_file, "</a>")
-  )|> 
+  dplyr::select(data_file, cite_link, notes) |> 
+  dplyr::mutate(
+    link = paste0("<a href='https://raw.githubusercontent.com/drmankin/practicum/master/data/", 
+                  data_file, "'>Download ", data_file, "</a>")
+  ) |> 
   dplyr::arrange(data_file)
+# 
+# data_tab <- tibble::tibble(
+#   data_file = list.files(data_path, pattern = "data\\..*?$"),
+#   link = paste0("<a href='https://raw.githubusercontent.com/drmankin/practicum/master/data/", 
+#                 data_file, "'>Download ", data_file, "</a>")
+#   )|> 
+#   dplyr::arrange(data_file)
 
 data_tab <- data_info |>
-  dplyr::left_join(data_tab) |> 
+  #dplyr::left_join(data_tab) |> 
   kableExtra::kbl(
     col.names = c("Filename", "Citation/Source", "Comments", "Download"),
     format = "html",
@@ -60,7 +66,7 @@ wkbk_tab <- tibble::tibble(
   ) |> 
   dplyr::select(title, link) |> 
   kableExtra::kbl(
-    col.names = c("Tutorial", "Download"),
+    col.names = c("Tutorial/Workshop Name", "Download"),
     format = "html",
     escape = FALSE
   ) |> 
@@ -77,7 +83,8 @@ writeLines(
     "## Datasets",
     "\n",
     "Download and save datasets to use for tutorial tasks here.",
-    "Either copy the link to use in `readr::read_csv()`, or save the data at the link to a .csv file to read in.",
+    "Either copy the link to use in a reading-in function (e.g. `readr::read_csv()`, `haven::read_sav()`), or save the data at the link to a file to read in.",
+    "Make sure that if you save the file, you keep the correct file suffix!",
     "\n",
     data_tab,
     "\n",
